f177ae60a5bf106d4765cb286fc66a0380fa3437
Oleg-Tim-Pro <128182858+Oleg-Tim-Pro@users.noreply.github.com>
2023-06-16 10:15:42 +0300

    Update and rename ProcessPatient.php to PerformPatient.php

--- PerformPatient.php
+++ PerformPatient.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
 
-class ProcessPatient implements ShouldQueue
+class PerformPatient implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
