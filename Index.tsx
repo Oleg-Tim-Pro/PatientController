ce5bce3fcc9fa258bc8b1f99ca3f4edc6a22b18a
Oleg-Tim-Pro <128182858+Oleg-Tim-Pro@users.noreply.github.com>
2023-06-16 11:08:03 +0300

    Код представления Index.tsx

--- Index.tsx
+++ Index.tsx
@@ -16,14 +16,7 @@ interface Props {
 const Index: React.FC<Props> = ({ patients }) => {
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-2xl font-semibold mb-4">Patients</h1>
-
-      <div className="mb-4">
-        <InertiaLink href="/patients/create" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Create Patient
-        </InertiaLink>
-      </div>
-
+    
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
