1e58fdf0c468416b720ff11067bfea26a1ff0d6b
Oleg-Tim-Pro <128182858+Oleg-Tim-Pro@users.noreply.github.com>
2023-06-16 12:14:36 +0300

    Код представления Index.tsx

--- Index.tsx
+++ Index.tsx
@@ -1,46 +1,45 @@
 import React from 'react';
-import { InertiaLink } from '@inertiajs/inertia-react';
 import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper } from '@mui/material';
 
 interface Patient {
-  id: number;
-  name: string;
-  birthdate: string;
-  age: string;
+    id: number;
+    name: string;
+    birthdate: string;
+    age: string;
 }
 
 interface Props {
-  patients: Patient[];
+    patients: Patient[];
 }
 
 const Index: React.FC<Props> = ({ patients }) => {
-  return (
-    <div className="max-w-4xl mx-auto p-4">
-    
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Birthdate</TableCell>
-              <TableCell>Age</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {patients.map((patient) => (
-              <TableRow key={patient.id}>
-                <TableCell>{patient.id}</TableCell>
-                <TableCell>{patient.name}</TableCell>
-                <TableCell>{patient.birthdate}</TableCell>
-                <TableCell>{patient.age}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  );
+    return (
+        <div className="max-w-4xl mx-auto p-4">
+
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>№ Клиента</TableCell>
+                            <TableCell>ФИО</TableCell>
+                            <TableCell>Дата рождения</TableCell>
+                            <TableCell>Возраст</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {patients.map((patient) => (
+                            <TableRow key={patient.id}>
+                                <TableCell>{patient.id}</TableCell>
+                                <TableCell>{patient.name}</TableCell>
+                                <TableCell>{patient.birthdate}</TableCell>
+                                <TableCell>{patient.age}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
 };
 
 export default Index;
