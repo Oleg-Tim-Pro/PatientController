369f865969faa11995a898eac549f2da45ef9d63
Oleg-Tim-Pro <128182858+Oleg-Tim-Pro@users.noreply.github.com>
2023-06-16 10:55:03 +0300

    php artisan make: model Patient -mfsc

--- PatientController.php
+++ PatientController.php
@@ -1,11 +1,13 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 //PerformPatient - используется для обработки сущности в очереди
 use App\Jobs\PerformPatient;
 use App\Models\Patient;
 use Illuminate\Http\Request;
+// Inertia.js - это библиотека для работы с React, Vue
+use Inertia\Inertia;
 // Carbon - библиотека для работы с датами
 use Carbon\Carbon;
 // Cache - работа с кешом
@@ -13,24 +15,24 @@ use Cache;
 
 class PatientController extends Controller
 {
-   /**
-     * Метод для получения списка всех пациентов.
+     /**
+     * Получения списка всех пациентов.
      */
     public function index()
     {
         // Проверяем наличие списка пациентов в кеше
         if (Cache::has('patients')) {
             // Если список пациентов есть в кеше, возвращаем его из кеша
-            return Cache::get('patients');
+            $patients = Cache::get('patients');
         } else {
             // Если список пациентов отсутствует в кеше, получаем всех пациентов из базы данных
             $patients = Patient::all();
             foreach ($patients as &$patient) {
-                // Добавляем поле name  соединяем first_name и last_name
+                // Добавляем поле name объединяем first_name и last_name
                 $patient->name = $patient->first_name . ' ' . $patient->last_name;
                 // Форматируем дату рождения в формат день.месяц.год
                 $patient->birthdate = Carbon::parse($patient->birthdate)->format('d.m.Y');
-                // Добавляем единицу измерения к возрасту (д/м/г)
+                // Добавляем единицу измерения к возрасту день.месяц.год
                 if ($patient->age_type == 'день') {
                     $patient->age .= ' день';
                 } elseif ($patient->age_type == 'месяц') {
@@ -39,19 +41,35 @@ class PatientController extends Controller
                     $patient->age .= ' год';
                 }
             }
-            // Сохраняем в кеше на 5 минут
+            // Сохраняем список пациентов в кеше на 5 минут
             Cache::put('patients', $patients, 300);
-            return $patients;
         }
+
+        return Inertia::render('Patients/Index', ['patients' => $patients]);
     }
-  
+ 
     /**
-     * Метод для создания нового пациента.
+     * Отображения формы создания пациента.
+     */
+    public function create()
+    {
+        return Inertia::render('Patients/Create');
+    }
+
+    /**
+     * Сохранения нового пациента.
      */
     public function store(Request $request)
     {
+        // Валидация данных
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'birthdate' => 'required|date',
+        ]);
+
         $patient = new Patient;
-        // Заполнение полей first_name, last_name и birthdate
+        // Заполнение полей first_name, last_name и birthdate из запроса
         $patient->first_name = $request->first_name;
         $patient->last_name = $request->last_name;
         $patient->birthdate = $request->birthdate;
@@ -64,28 +82,31 @@ class PatientController extends Controller
             // Если возраст меньше года, то в месяцах
             $age = $birthdate->diffInMonths($now);
             if ($age < 1) {
-                // Если возраст меньше месяца, то возраст в днях
+                // Если возраст меньше месяца, то в днях
                 $age = $birthdate->diffInDays($now);
-                //  Возраста в денях
+                //  Возраст в днях
                 $patient->age_type = 'день';
             } else {
-                // Возраста в месяцах
+                // Возраст в месяцах
                 $patient->age_type = 'месяц';
             }
         } else {
-            // Возраста в годах
+            // Возраст в годах
             $patient->age_type = 'год';
         }
         // Сохраняем возраст в поле age
         $patient->age = $age;
 
-        // Сохраняем  в базу данных
+        // Сохраняем пациента в базе данных
         $patient->save();
 
-        // Сохраняем в кеше на 5 минут
+        // Сохраняем в кеше на 5 минут (300 секунд)
         Cache::put('patient_' . $patient->id, $patient, 300);
 
         // Отправка в очередь для обработки
         PerformPatient::dispatch($patient);
+
+        // Перенаправление на страницу со списком пациентов после создания
+        return redirect()->route('patients.index');
     }
 }
